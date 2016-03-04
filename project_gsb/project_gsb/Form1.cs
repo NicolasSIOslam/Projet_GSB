f5dd2fa145c3829e100a363fe5f2c85d41c9286a
Nicolas <nicolas.poincet@hotmail.fr>
2016-03-04 16:36:44 +0100

    premier commit

---
+++ project_gsb/project_gsb/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_gsb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int a = 1;
+
+            while (a < 10)
+            {
+                a++;
+            }
+        }
+    }
+}
