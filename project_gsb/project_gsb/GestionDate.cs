d30eea81d03c54864976549a370c90bd5f472b42
Jerome <jerome.ksi@gmail.com>
2016-03-05 15:37:33 +0100

    Ajout fonctions et commentaire, test unitaire

--- project_gsb/project_gsb/GestionDate.cs
+++ project_gsb/project_gsb/GestionDate.cs
@@ -8,10 +8,27 @@ namespace project_gsb
 {
     class GestionDate
     {
-        private int dateCourrant()
+        /// <summary>
+        /// Retourne le mois en cours
+        /// </summary>
+        /// <returns>int</returns>
+        public int moisCourrant()
         {
             return DateTime.Now.Month;
         }
 
+        /// <summary>
+        /// Retourne le mois d'avant le mois en cours
+        /// </summary>
+        /// <returns></returns>
+        public int moisPrecedent()
+        {
+            int mois = DateTime.Now.Month;
+            if (DateTime.Now.Month - 1 == 0)
+            {
+                mois = 12;
+            }
+            return mois;
+        }
     }
 }
